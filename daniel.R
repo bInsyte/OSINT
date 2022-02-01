4f491e77769879b13262aa84c8ecbfefedc603a0
snakewizardd <macbookpro@Macbooks-MacBook-Pro.local>
2022-02-01 18:54:15 -0500

    sample get request

--- daniel.R
+++ daniel.R
@@ -1,8 +1,14 @@
 library(readODS)
 library(tidyverse)
-
+library(httr)
 data <- read_ods('Zoose_Osint_v2.ods')
 
 #Fixing missing column names with placeholders
 colnames(data)[(which((colnames(data) == '')))] <- paste('Column_',(which((colnames(data) == ''))))
+
+#Parse list of all URLS
+sort(unique(data$URL))[1]
+
+#Making get request to first url in list
+GET(url=sort(unique(data$URL))[1])
 
